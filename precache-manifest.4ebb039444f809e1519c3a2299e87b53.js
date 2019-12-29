d0cbb013ffb1f5da4b9685327eb62ec6598af9f6
almog <almogbh.l@gmail.com>
2019-12-29 04:40:40 +0200

    Updates

---
+++ precache-manifest.4ebb039444f809e1519c3a2299e87b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcd38232271d1ecaab400de146b33b7",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "5a9765ecc3e4b451f9d4",
+    "url": "/weather_app/static/js/2.fa7d6672.chunk.js"
+  },
+  {
+    "revision": "2044111df0f0647258f42cd9dae94012",
+    "url": "/weather_app/static/js/2.fa7d6672.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c68656ed01628ca67188",
+    "url": "/weather_app/static/js/main.6e62c41f.chunk.js"
+  },
+  {
+    "revision": "71593ea389c627574084",
+    "url": "/weather_app/static/js/runtime-main.c13d2a06.js"
+  }
+]);
